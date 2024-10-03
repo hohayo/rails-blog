f3a578c931eb23db67279fe55440b731536606c5
Kyle Chuang <hohayo@gmail.com>
2024-10-03 12:04:13 +0800

    表單新增 status 選項

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,8 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 
   validates :commenter, presence: true
   validates :body, presence: true, length: { minimum: 5 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
